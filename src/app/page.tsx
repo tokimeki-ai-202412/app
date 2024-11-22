f7f737632041850674a3c9f5a855d3c311d3c032
aki-0421 <118268728+aki-0421@users.noreply.github.com>
2024-11-23 01:04:39 +0900

    F: update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,26 @@
 
 import { Button } from '@/components/ui/button';
 import { ColorModeButton } from '@/components/ui/color-mode';
-import { HStack } from '@chakra-ui/react';
+import {
+  useGetCharacter,
+  useListCharacters,
+} from '@/states/hooks/character.ts';
+import { useWhois } from '@/states/hooks/user.ts';
+import { Text, VStack } from '@chakra-ui/react';
 import type { ReactElement } from 'react';
 
 export default function Page(): ReactElement {
+  const { user } = useWhois();
+  const { characters } = useListCharacters();
+  const { character } = useGetCharacter('aaa');
+
   return (
-    <HStack>
+    <VStack>
       <ColorModeButton />
       <Button>Click me</Button>
-    </HStack>
+      <Text>{JSON.stringify(user)}</Text>
+      <Text>{JSON.stringify(characters)}</Text>
+      <Text>{JSON.stringify(character)}</Text>
+    </VStack>
   );
 }
