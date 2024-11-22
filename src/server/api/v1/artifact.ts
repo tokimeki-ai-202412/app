87dda18d119fc6cba2bb04fc2cc3b2d0e6d3c4eb
aki-0421 <118268728+aki-0421@users.noreply.github.com>
2024-11-22 21:31:12 +0900

    F: add artifact, character service

--- src/server/api/v1/artifact.ts
+++ src/server/api/v1/artifact.ts
@@ -10,6 +10,8 @@ import type {
   ListArtifactsRequest,
   ListArtifactsResponse,
 } from '@/libraries/connect-gen/api/v1/artifact/api_pb';
+import type { Artifact } from '@/libraries/connect-gen/model/v1/artifact_pb.ts';
+import { contextKeyPrisma, contextKeyUserId } from '@/server/context.ts';
 import { Code, ConnectError, type HandlerContext } from '@connectrpc/connect';
 
 const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
@@ -41,15 +43,62 @@ export const deleteArtifact: (
 export const getArtifact: (
   req: GetArtifactRequest,
   ctx: HandlerContext,
-) => Promise<GetArtifactResponse> = async () => {
-  await wait(100);
-  return {} as GetArtifactResponse;
+) => Promise<GetArtifactResponse> = async (req, ctx) => {
+  const userId = ctx.values.get(contextKeyUserId);
+  if (!userId) {
+    throw new ConnectError('Unauthenticated', Code.Unauthenticated);
+  }
+  const prisma = ctx.values.get(contextKeyPrisma);
+  if (!prisma) {
+    throw new ConnectError('Internal Error', Code.Internal);
+  }
+
+  const artifact = await prisma.artifact.findFirstOrThrow({
+    where: {
+      id: req.artifactId,
+      userId,
+    },
+  });
+
+  // R2からartifact idのフォルダの中身をリストにしてすべてのファイルに署名する実装を追加する
+  return {
+    artifact: {
+      id: artifact.id,
+      jobId: artifact.jobId,
+      status: artifact.status || 'ERROR',
+    },
+  } as GetArtifactResponse;
 };
 
 export const listArtifacts: (
   req: ListArtifactsRequest,
   ctx: HandlerContext,
-) => Promise<ListArtifactsResponse> = async () => {
-  await wait(100);
-  return {} as ListArtifactsResponse;
+) => Promise<ListArtifactsResponse> = async (req, ctx) => {
+  const userId = ctx.values.get(contextKeyUserId);
+  if (!userId) {
+    throw new ConnectError('Unauthenticated', Code.Unauthenticated);
+  }
+  const prisma = ctx.values.get(contextKeyPrisma);
+  if (!prisma) {
+    throw new ConnectError('Internal Error', Code.Internal);
+  }
+
+  const artifacts = await prisma.artifact.findMany({
+    where: {
+      characterId: req.characterId,
+      userId,
+    },
+  });
+
+  // R2からartifact idのフォルダの中身をリストにしてすべてのファイルに署名する実装を追加する
+  const results: Partial<Artifact>[] = artifacts.map((artifact) => ({
+    id: artifact.id,
+    jobId: artifact.jobId,
+    status: artifact.status || 'ERROR',
+    object_urls: [],
+  }));
+
+  return {
+    artifacts: results,
+  } as ListArtifactsResponse;
 };
--- src/server/api/v1/character.ts
+++ src/server/api/v1/character.ts
@@ -10,6 +10,8 @@ import type {
   UpdateCharacterRequest,
   UpdateCharacterResponse,
 } from '@/libraries/connect-gen/api/v1/character/api_pb';
+import type { Character } from '@/libraries/connect-gen/model/v1/character_pb.ts';
+import { contextKeyPrisma, contextKeyUserId } from '@/server/context.ts';
 import { Code, ConnectError, type HandlerContext } from '@connectrpc/connect';
 
 const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
@@ -17,39 +19,150 @@ const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 export const createCharacter: (
   req: CreateCharacterRequest,
   ctx: HandlerContext,
-) => Promise<CreateCharacterResponse> = async () => {
-  await wait(100);
-  return {} as CreateCharacterResponse;
+) => Promise<CreateCharacterResponse> = async (req, ctx) => {
+  const userId = ctx.values.get(contextKeyUserId);
+  if (!userId) {
+    throw new ConnectError('Unauthenticated', Code.Unauthenticated);
+  }
+  const prisma = ctx.values.get(contextKeyPrisma);
+  if (!prisma) {
+    throw new ConnectError('Internal Error', Code.Internal);
+  }
+
+  if (req.name === '') {
+    throw new ConnectError('Invalid Argument', Code.InvalidArgument);
+  }
+
+  const character = await prisma.character.create({
+    data: {
+      name: req.name,
+      thumbnailPath: req.thumbnailPath,
+      userId,
+    },
+  });
+
+  // thumbnailPathが存在する場合、signed urlを発行する
+
+  return {
+    character: {
+      id: character.id,
+      name: character.name,
+      thumbnailPath: character.thumbnailPath,
+    },
+  } as CreateCharacterResponse;
 };
 
 export const deleteCharacter: (
   req: DeleteCharacterRequest,
   ctx: HandlerContext,
-) => Promise<DeleteCharacterResponse> = async () => {
-  await wait(100);
+) => Promise<DeleteCharacterResponse> = async (req, ctx) => {
+  const userId = ctx.values.get(contextKeyUserId);
+  if (!userId) {
+    throw new ConnectError('Unauthenticated', Code.Unauthenticated);
+  }
+  const prisma = ctx.values.get(contextKeyPrisma);
+  if (!prisma) {
+    throw new ConnectError('Internal Error', Code.Internal);
+  }
+
+  const character = await prisma.character.findFirstOrThrow({
+    where: {
+      id: req.characterId,
+    },
+  });
+
+  if (character.userId !== userId) {
+    throw new ConnectError('Unauthenticated', Code.Unauthenticated);
+  }
+
+  await prisma.character.delete({
+    where: {
+      id: req.characterId,
+    },
+  });
+
   return {} as DeleteCharacterResponse;
 };
 
 export const getCharacter: (
   req: GetCharacterRequest,
   ctx: HandlerContext,
-) => Promise<GetCharacterResponse> = async () => {
-  await wait(100);
-  return {} as GetCharacterResponse;
+) => Promise<GetCharacterResponse> = async (req, ctx) => {
+  const userId = ctx.values.get(contextKeyUserId);
+  if (!userId) {
+    throw new ConnectError('Unauthenticated', Code.Unauthenticated);
+  }
+  const prisma = ctx.values.get(contextKeyPrisma);
+  if (!prisma) {
+    throw new ConnectError('Internal Error', Code.Internal);
+  }
+
+  const character = await prisma.character.findFirstOrThrow({
+    where: {
+      id: req.characterId,
+    },
+  });
+
+  if (character.userId !== userId) {
+    throw new ConnectError('Unauthenticated', Code.Unauthenticated);
+  }
+
+  // thumbnailPathが存在する場合、signed urlを発行する
+
+  return {
+    character: {
+      id: character.id,
+      name: character.name,
+      thumbnailPath: character.thumbnailPath,
+    },
+  } as GetCharacterResponse;
 };
 
 export const listCharacters: (
   req: ListCharactersRequest,
   ctx: HandlerContext,
-) => Promise<ListCharactersResponse> = async () => {
-  await wait(100);
-  return {} as ListCharactersResponse;
+) => Promise<ListCharactersResponse> = async (_, ctx) => {
+  const userId = ctx.values.get(contextKeyUserId);
+  if (!userId) {
+    throw new ConnectError('Unauthenticated', Code.Unauthenticated);
+  }
+  const prisma = ctx.values.get(contextKeyPrisma);
+  if (!prisma) {
+    throw new ConnectError('Internal Error', Code.Internal);
+  }
+
+  const characters = await prisma.character.findMany({
+    where: {
+      userId,
+    },
+  });
+
+  // thumbnailPathが存在する場合、signed urlを発行する
+
+  const results: Partial<Character>[] = characters.map((character) => ({
+    id: character.id,
+    name: character.name,
+    thumbnailPath: character.thumbnailPath,
+  }));
+
+  return {
+    characters: results,
+  } as ListCharactersResponse;
 };
 
 export const updateCharacter: (
   req: UpdateCharacterRequest,
   ctx: HandlerContext,
-) => Promise<UpdateCharacterResponse> = async () => {
+) => Promise<UpdateCharacterResponse> = async (_, ctx) => {
+  const userId = ctx.values.get(contextKeyUserId);
+  if (!userId) {
+    throw new ConnectError('Unauthenticated', Code.Unauthenticated);
+  }
+  const prisma = ctx.values.get(contextKeyPrisma);
+  if (!prisma) {
+    throw new ConnectError('Internal Error', Code.Internal);
+  }
+
   await wait(100);
   return {} as UpdateCharacterResponse;
 };
