3f70ab4a052434e062413c15abf95606c35ce13b
aki-0421 <118268728+aki-0421@users.noreply.github.com>
2024-12-08 14:49:58 +0900

    R: refactor new artifact page

--- src/app/characters/[characterId]/new/page.tsx
+++ src/app/characters/[characterId]/new/page.tsx
@@ -10,6 +10,11 @@ import {
   SelectTrigger,
   SelectValueText,
 } from '@/components/ui/select';
+import {
+  StepsCompletedContent,
+  StepsContent,
+  StepsRoot,
+} from '@/components/ui/steps';
 import { modelData } from '@/const/model.ts';
 import { API } from '@/libraries/connect-client';
 import { useListArtifact } from '@/states/hooks/artifact.ts';
@@ -18,11 +23,12 @@ import {
   AspectRatio,
   Box,
   Container,
+  Flex,
+  Grid,
   GridItem,
-  Heading,
+  HStack,
   Image,
-  SimpleGrid,
-  Spacer,
+  List,
   Text,
   VStack,
 } from '@chakra-ui/react';
@@ -31,11 +37,30 @@ import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { type ReactElement, useState } from 'react';
 
+function getModelNameById(id: string): string {
+  const model = modelData.find((m) => m.id === id);
+  if (!model) {
+    return '不明なモデル';
+  }
+  return model.name;
+}
+
 const models = createListCollection({
   items: modelData.map((model) => ({
     label: model.name,
     value: model.id,
   })),
+});
+
+const elevations = createListCollection({
+  items: [
+    { label: '-10 (下から)', value: '-10' },
+    { label: '0 (正面)', value: '0' },
+    { label: '10', value: '10' },
+    { label: '20', value: '20' },
+    { label: '30', value: '30' },
+    { label: '40 (上から)', value: '40' },
+  ],
 });
 
 export const runtime = 'edge';
@@ -49,9 +74,13 @@ type Props = {
 export default function Page({ params: { characterId } }: Props): ReactElement {
   const { user, refresh: refreshUser } = useWhois();
   const { refresh } = useListArtifact(characterId);
-  const [thumbnail, setThumbnail] = useState<string>('/sample.png');
-  const [modelName, setModelName] = useState<string[]>([modelData[1].id]);
+
   const [file, setFile] = useState<File | null>(null);
+  const [thumbnail, setThumbnail] = useState<string>('/sample.png');
+
+  const [modelName, setModelName] = useState<string[]>([modelData[1].id]);
+  const [elevation, setElevation] = useState<string[]>(['0']);
+
   const [loading, setLoading] = useState<boolean>(false);
 
   const handleFileChange = (e: any) => {
@@ -92,6 +121,7 @@ export default function Page({ params: { characterId } }: Props): ReactElement {
           modelName: modelName[0],
         },
         characterId: characterId,
+        //elevation: elevation[0],
       });
       if (!artifact) throw new Error('api error');
 
@@ -107,110 +137,230 @@ export default function Page({ params: { characterId } }: Props): ReactElement {
     setLoading(false);
   }
 
+  const [step, setStep] = useState<number>(0);
+
   return (
-    <Container py={4} maxW={{ base: '1024px' }}>
-      <SimpleGrid pb={8} gap={4}>
-        <GridItem>
-          <Heading
-            fontSize={{ base: '24px', lg: '32px' }}
-            color="blackAlpha.700"
-            textAlign="center"
-          >
-            キャラクターに画像を追加
-          </Heading>
-        </GridItem>
-        <GridItem>
-          <Text textAlign="center" opacity={0.6}>
-            （推奨）背景なし・1024px以上の正方形
-          </Text>
-        </GridItem>
-      </SimpleGrid>
-      <SimpleGrid columns={{ base: 1, lg: 2 }} gap={8}>
-        <GridItem>
-          <Box
-            borderWidth="1px"
-            borderColor="blackAlpha.100"
-            borderRadius="8px"
-            bg="blackAlpha.50"
-          >
-            <Box as="label" w="full" cursor="pointer">
-              <AspectRatio w="full" h="full" ratio={1}>
-                <Image
-                  src={thumbnail}
-                  opacity={thumbnail === '/sample.png' ? 0.15 : 1}
-                  style={{ objectFit: 'contain' }}
-                />
-              </AspectRatio>
-              <input
-                type="file"
-                accept="image/*"
-                style={{
-                  display: 'none',
-                }}
-                onChange={handleFileChange}
-              />
-            </Box>
-          </Box>
-        </GridItem>
-        <GridItem>
-          <VStack w="full" h="full" gap={8}>
-            <SimpleGrid w="full" columns={1} gap={8}>
-              <GridItem>
-                <SelectRoot
+    <Container maxW={{ base: '768px' }}>
+      <VStack mb={12} gap={12}>
+        <Box w="full">
+          <StepsRoot size="lg" count={2} step={step}>
+            <StepsContent index={0}>
+              <VStack gap={8}>
+                <Box
+                  p={4}
                   w="full"
-                  collection={models}
-                  value={modelName}
-                  onValueChange={(e) => setModelName(e.value)}
+                  borderWidth="1px"
+                  borderColor="blackAlpha.100"
+                  borderRadius="8px"
                 >
-                  <SelectLabel fontSize="0.9em">モデル</SelectLabel>
-                  <SelectTrigger>
-                    <SelectValueText placeholder="モデルを選んでください" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    {models.items.map((model) => (
-                      <SelectItem item={model} key={model.value}>
-                        {model.label}
-                      </SelectItem>
-                    ))}
-                  </SelectContent>
-                </SelectRoot>
-              </GridItem>
-              <GridItem>
-                <Button disabled={true}>参考にするポーズを指定する</Button>
-              </GridItem>
-            </SimpleGrid>
-            <Spacer />
-            <SimpleGrid w="full" gap={4}>
-              {user && user.jewelRemain < 1 && (
-                <GridItem>
-                  <Alert status="info" title="1024pxでの生成について">
-                    現在、1人あたりの使用回数を制限しています。
-                    <Link
-                      href="https://forms.gle/N4mQTi1ytGL51JnYA"
-                      target="_blank"
+                  <List.Root>
+                    <List.Item>1024px以上のほぼ正方形な画像を使う</List.Item>
+                    <List.Item>なるべくフラットなイラストを使う</List.Item>
+                  </List.Root>
+                </Box>
+                <Box
+                  w="full"
+                  borderWidth="1px"
+                  borderColor="blackAlpha.100"
+                  borderRadius="8px"
+                  bg="blackAlpha.50"
+                >
+                  <Box as="label" w="full" cursor="pointer">
+                    <AspectRatio w="full" h="full" ratio={1}>
+                      <Image
+                        src={thumbnail}
+                        opacity={thumbnail === '/sample.png' ? 0.15 : 1}
+                        style={{ objectFit: 'contain' }}
+                      />
+                    </AspectRatio>
+                    <input
+                      type="file"
+                      accept="image/*"
+                      style={{
+                        display: 'none',
+                      }}
+                      onChange={handleFileChange}
+                    />
+                  </Box>
+                </Box>
+                <Flex w="full" gap={8}>
+                  <Button w="full" size="2xl" onClick={() => setStep(1)}>
+                    次へ
+                  </Button>
+                </Flex>
+              </VStack>
+            </StepsContent>
+            <StepsContent index={1}>
+              <VStack gap={8}>
+                <Box w="full">
+                  <SelectRoot
+                    w="full"
+                    size="lg"
+                    collection={models}
+                    value={modelName}
+                    onValueChange={(e) => setModelName(e.value)}
+                  >
+                    <SelectLabel fontSize="0.9em">モデル</SelectLabel>
+                    <SelectTrigger>
+                      <SelectValueText placeholder="モデルを選んでください" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {models.items.map((model) => (
+                        <SelectItem item={model} key={model.value}>
+                          {model.label}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </SelectRoot>
+                </Box>
+                <Box w="full">
+                  <SelectRoot
+                    w="full"
+                    size="lg"
+                    collection={elevations}
+                    value={elevation}
+                    onValueChange={(e) => setElevation(e.value)}
+                  >
+                    <SelectLabel fontSize="0.9em">角度</SelectLabel>
+                    <SelectTrigger>
+                      <SelectValueText placeholder="角度を選んでください" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {elevations.items.map((elevation) => (
+                        <SelectItem item={elevation} key={elevation.value}>
+                          {elevation.label}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </SelectRoot>
+                </Box>
+                <Box w="full">
+                  <Button w="full" disabled={true}>
+                    参考にするポーズを指定する
+                  </Button>
+                </Box>
+                <Grid
+                  w="full"
+                  templateColumns="repeat(5, 1fr)"
+                  gap={{ base: 4, lg: 8 }}
+                >
+                  <GridItem colSpan={2}>
+                    <Button
+                      w="full"
+                      size="2xl"
+                      color="blackAlpha.700"
+                      bg="transparent"
+                      borderWidth="1px"
+                      borderColor="blackAlpha.100"
+                      borderRadius="8px"
+                      onClick={() => setStep(0)}
                     >
-                      <Text color="#f0acac" fontWeight={700}>
-                        今後にご期待ください。
-                      </Text>
-                    </Link>
-                  </Alert>
-                </GridItem>
-              )}
-              <GridItem>
-                <Button
-                  w="full"
-                  size="2xl"
-                  loading={loading}
-                  disabled={file === null || loading || !user}
-                  onClick={createJob}
-                >
-                  生成する
-                </Button>
-              </GridItem>
-            </SimpleGrid>
-          </VStack>
-        </GridItem>
-      </SimpleGrid>
+                      戻る
+                    </Button>
+                  </GridItem>
+                  <GridItem colSpan={3}>
+                    <Button w="full" size="2xl" onClick={() => setStep(2)}>
+                      次へ
+                    </Button>
+                  </GridItem>
+                </Grid>
+              </VStack>
+            </StepsContent>
+            <StepsCompletedContent>
+              <VStack gap={8}>
+                <Box w="full">
+                  <HStack w="full" gap={4}>
+                    <Box>
+                      <Box as="label" w="full" h="full" cursor="pointer">
+                        <Box
+                          borderWidth="1px"
+                          borderColor="blackAlpha.50"
+                          borderRadius="8px"
+                        >
+                          <AspectRatio
+                            boxSize={{ base: '128px', lg: '160px' }}
+                            ratio={1}
+                          >
+                            <Image
+                              src={thumbnail}
+                              opacity={thumbnail === '/sample.png' ? 0.15 : 1}
+                              style={{ objectFit: 'contain' }}
+                              userSelect="none"
+                              pointerEvents="none"
+                            />
+                          </AspectRatio>
+                        </Box>
+                        <Button
+                          style={{
+                            display: 'none',
+                          }}
+                          onClick={() => {
+                            setStep(0);
+                          }}
+                        />
+                      </Box>
+                    </Box>
+                    <Box w="full">
+                      <VStack w="full" align="flex-start" h="full">
+                        <Flex gap={2}>
+                          <Text>モデル</Text>
+                          <Box as="label" cursor="pointer">
+                            <Text color="#f0acac" fontWeight={700}>
+                              {getModelNameById(modelName[0])}
+                            </Text>
+                            <Button
+                              style={{
+                                display: 'none',
+                              }}
+                              onClick={() => {
+                                setStep(1);
+                              }}
+                            />
+                          </Box>
+                        </Flex>
+                      </VStack>
+                    </Box>
+                  </HStack>
+                </Box>
+
+                {user && user.jewelRemain < 1 && (
+                  <Box w="full">
+                    <Alert
+                      color="blackAlpha.700"
+                      bg="transparent"
+                      borderWidth="1px"
+                      borderColor="blackAlpha.100"
+                      borderRadius="8px"
+                      title="1024pxでの生成について"
+                    >
+                      現在、1人あたりの使用回数を制限しています。
+                      <Link
+                        href="https://forms.gle/N4mQTi1ytGL51JnYA"
+                        target="_blank"
+                      >
+                        <Text color="#f0acac" fontWeight={700}>
+                          今後にご期待ください。
+                        </Text>
+                      </Link>
+                    </Alert>
+                  </Box>
+                )}
+                <Box w="full">
+                  <Button
+                    w="full"
+                    size="2xl"
+                    loading={loading}
+                    disabled={file === null || loading || !user}
+                    onClick={createJob}
+                  >
+                    生成する
+                  </Button>
+                </Box>
+              </VStack>
+            </StepsCompletedContent>
+          </StepsRoot>
+        </Box>
+      </VStack>
     </Container>
   );
 }
